2c9b61a7ae41c637c11ddb95a0c99779179f8384
PariyugSpray <dasarwarpariyug30@gmail.com>
2026-02-13 21:33:44 +0530

    Create User.js

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+});
+
+UserSchema.pre('save', function(next) {
+    this.updatedAt = Date.now();
+    next();
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
